5765b030995bb799e5869faceece9d92df7ec746
jorge Luis Pena Real <falmemor@gmail.com>
2022-02-27 23:36:10 -0600

    added db

--- src/components/home.component.tsx
+++ src/components/home.component.tsx
@@ -1,8 +1,16 @@
 import { FC } from "react";
 import { axiosClient } from "../axios/axiosclient";
+import Box from "@mui/material/Box";
 
 export const HomeComponent: FC = (): JSX.Element => {
   const datos = axiosClient.get("/posts");
   datos.then((e) => console.log(e.data));
-  return <div>home.component</div>;
+  return (
+    <Box
+      sx={{
+        width: "100vw",
+        height: "100vh",
+      }}
+    ></Box>
+  );
 };
