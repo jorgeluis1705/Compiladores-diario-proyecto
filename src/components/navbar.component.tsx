1279c932b7049d1fb257f2c3d908f4bc26694e45
jorge Luis Pena Real <falmemor@gmail.com>
2022-02-27 23:17:49 -0600

    navbar

--- src/components/navbar.component.tsx
+++ src/components/navbar.component.tsx
@@ -1,21 +1,17 @@
-import { Link } from "react-router-dom";
-import { useStyles } from "./navbar.styles";
+import * as React from "react";
 import AppBar from "@mui/material/AppBar";
 import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
-import Button from "@mui/material/Button";
 import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
-const NavBar = () => {
-  const classes: any = useStyles();
 
+const NavBar = () => {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
-        <Toolbar>
+        <Toolbar variant="dense">
           <IconButton
-            size="large"
             edge="start"
             color="inherit"
             aria-label="menu"
@@ -23,10 +19,9 @@ const NavBar = () => {
           >
             <MenuIcon />
           </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            News
+          <Typography variant="h6" color="inherit" component="div">
+            Diario de clase
           </Typography>
-          <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
     </Box>
