0e9a90a6a4eef7929423e96b61f5ee3ab6cf7991
Jorge Luis Pena Real <jorgeluispenareal@gmail.com>
2022-06-13 23:59:14 -0500

    added fina;l

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,25 @@ import { Parcial1Component } from "./components/parcial-1/parcial1.component";
 import { ProyectComponent } from "./components/proyecto/proyect.component";
 import cls from "./db.json";
 import P2 from "./parcial_2.json";
+import p3 from "./parcial_3.json";
 
 function App() {
   const router = useNavigate();
-  useEffect(() => {
-    router("/proyecto");
-  }, []);
+
   return (
     <Fragment>
       {/*    <NavBar /> */}
       <Routes>
-        <Route path="" element={<HomeComponent />} />
         <Route
-          path="/p1"
+          path="/aaaa"
           element={
             <Parcial1Component CLASSESPROP={cls.classes} title={"Parcial 1"} />
+          }
+        />
+        <Route
+          path="/"
+          element={
+            <Parcial1Component CLASSESPROP={p3.classes} title={"Parcial 3"} />
           }
         />
         <Route
